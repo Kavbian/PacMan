e532bd279b6e5ea7a2ab5bb959d52c85c7c0f77d
Kavbian <kavbian@gmail.com>
2025-11-26 17:19:56 +0100

    V1.1

--- scripts/main.js
+++ scripts/main.js
@@ -13,12 +13,29 @@ let animationData = {
 };
 
 class Collectibles {
-    constructor(x, y, width, height) {
+    constructor(x = 0, y = 0, width = 25, height = 25) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.visible = true;
+    }
+
+    draw() {
+        if(this.visible) {
+            ctx.fillStyle = "#000000"
+            ctx.fillRect(this.x, this.y, this.width, this.height)
+        }
+    }
+
+    collision(character) {
+        if (this.visible) {
+            if ((character.x + character.width > this.x && x < this.x + this.width) &&
+                (character.y + character.height > this.y && y < this.y + this.height)
+            ) {
+                this.visible = false;
+            }
+        }
     }
 }
 
@@ -30,113 +47,110 @@ class Character {
         this.width = width;
         this.height = height;
 
-        const moveData = {
+        this.moveData = {
             speed: 5,
             speedMultiplier: 1,
             direction: "right",
         };
+
+        this.animationData = {
+            currentGapSize: 10,
+            maxGapSize: 30,
+            gapIncrement: 1,
+            gapIncrementMultiplier: 1,
+        };
     }
 
     move() {
+        let distance = this.moveData.speed * this.moveData.speedMultiplier;
         switch (this.moveData.direction) {
             case "right":
-                this.x += this.moveData.speed * this.moveData.speedMultiplier;
+                this.x += distance;
                 break;
             case "left":
-                this.x -= this.moveData.speed * this.moveData.speedMultiplier;
+                this.x -= distance;
                 break;
             case "up":
-                this.y -= this.moveData.speed * this.moveData.speedMultiplier;
+                this.y -= distance;
                 break;
             case "down":
-                this.y += this.moveData.speed * this.moveData.speedMultiplier;
+                this.y += distance;
                 break;
         }
+
+        if (this.x < 0) {    
+            this.x = canvas.width - this.width;
+        }
+        else if (this.y < 0) {
+            this.y = canvas.height - this.height;
+        }
+        else if (this.x + this.width > canvas.width) {
+            this.x = 0;
+        }
+        else if (this.y + this.height > canvas.height) {
+            this.y = 0;
+        }
+    }
+
+    draw() {
+        let singleRectHeight = this.height / 2;
+        let halfGapSize = this.animationData.currentGapSize / 2;
+        let reducedRectHeight = singleRectHeight - halfGapSize;
+        this.animationData.currentGapSize += 
+            this.animationData.gapIncrement * this.animationData.gapIncrementMultiplier;
+
+        if (this.animationData.currentGapSize >= this.animationData.maxGapSize) {
+            this.animationData.gapIncrementMultiplier = -1;
+        }
+        else if (this.animationData.currentGapSize == 0) {
+            this.animationData.gapIncrementMultiplier = 1;
+        }
+
+        ctx.fillStyle = "#00ff00";
+        ctx.fillRect(this.x, this.y, this.width, reducedRectHeight);
+        ctx.fillStyle = "#ff0000";
+        ctx.fillRect(this.x, this.y + singleRectHeight * 2, this.width, reducedRectHeight * -1);
     }
 }
 
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
-    // if (pointA.visible) {
-    //     if ((x + width > pointA.x && x < pointA.x + pointA.width) &&
-    //         (y + height > pointA.y && y < pointA.y + pointA.height)
-    //     ) {
-    //         pointA.visible = false;
-    //     }
-    //     ctx.fillStyle = "#000000ff";
-    //     ctx.fillRect(pointA.x, pointA.y, pointA.width, pointA.height);
-    // }
+
 
     packMan.move();
-    draw();
+    packMan.draw();
 
+    point_one.draw(packMan);
+    point_one.collision(packMan);
 
+    point_two.collision(packMan);
+    point_two.draw(packMan);
 
     requestAnimationFrame(gameLoop);
 }
 
-function draw() {
-    if (x < 0) {
-        x = canvas.width - width;
-    }
-    else if (y < 0) {
-        y = canvas.height - height;
-    }
-    else if (x + width > canvas.width) {
-        x = 0;
-    }
-    else if (y + height > canvas.height) {
-        y = 0;
-    }
-
-
-    let singleRectHeight = height / 2;
-    let halfGapSize = animationData.currentGapSize / 2;
-    let reducedRectHeight = singleRectHeight - halfGapSize;
-    animationData.currentGapSize += 
-        animationData.gapIncrement * animationData.gapIncrementMultiplier;
-
-    if (animationData.currentGapSize >= animationData.maxGapSize) {
-        animationData.gapIncrementMultiplier = -1;
-    }
-    else if (animationData.currentGapSize == 0) {
-        animationData.gapIncrementMultiplier = 1;
-    }
-
-    ctx.fillStyle = "#00ff00";
-    ctx.fillRect(x, y, width, reducedRectHeight);
-    ctx.fillStyle = "#ff0000";
-    ctx.fillRect(x, y + singleRectHeight * 2, width, reducedRectHeight * -1);
-}
-
-const setVelocity = () => {
-    xVelocity = 1;
-}
-
-const clearVelocity = () => {
-    xVelocity = 0;
-}
-
 const packMan = new Character(100, 400)
+const point_one = new Collectibles(canvas.width / 2 - 100, canvas.height / 2);
+const point_two = new Collectibles(canvas.width / 2 + 100, canvas.height / 2);
 requestAnimationFrame(gameLoop);
 
 document.addEventListener("keydown", (event) => {
     switch (event.key) {
         case "ArrowRight":
-            moveData.direction = "right";
+            packMan.moveData.direction = "right";
             break;
         
         case "ArrowLeft":
-            moveData.direction = "left";
+            packMan.moveData.direction = "left";
             break;
 
         case "ArrowUp":
-            moveData.direction = "up";
+            packMan.moveData.direction = "up";
             break;
 
         case "ArrowDown":
-            moveData.direction = "down";
+            packMan.moveData.direction = "down";
             break;
     }
 })
